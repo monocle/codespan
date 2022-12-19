99825c3f0dd825b5f7d1369da6117d8b0821d651
monocle <monocle.github@gmail.com>
2022-12-19 13:41:10 -0800

    Enable JS span insertion
    With match:
    - keywords
    - declarators
    - punctuations
    - quotes
    - brackets

--- src/index.js
+++ src/index.js
@@ -5,17 +5,51 @@ import {
   punctuations,
   quotes,
   brackets,
-} from "../src/definitions.js";
+} from "../src/definitions-js.js";
 
-export default function jsAddSpans(text) {
-  let options = {};
+export function jsAddSpans(text) {
+  let options = { concat: [], typeMap: {} };
 
   options.typeMap = {
-    bracket: brackets,
+    keyword: keywords,
+    declarator: declarators,
+    punctuation: punctuations,
     quote: quotes,
+    bracket: brackets,
   };
 
+  quotes.forEach((quoteChar) => {
+    options.concat.push({
+      type: "string",
+      start: `${quoteChar}`,
+      stop: `${quoteChar}`,
+    });
+  });
+
+  options.concat.push({
+    type: "comment",
+    start: "//",
+    stop: "\n",
+    includeStartDelimeter: true,
+  });
+
+  options.concat.push({
+    type: "comment",
+    start: "/*",
+    stop: "*/",
+    includeStartDelimeter: true,
+    includeStopDelimeter: true,
+  });
+
   return stringParse(text, options).reduce((html, { type, value }) => {
-    return (html += `<span class="codespan-${type}">${value}</span>`);
+    return (html += `<span class="codespan-js-${type}">${value}</span>`);
   }, "");
+}
+
+export default function addSpans() {
+  const codeElems = document.querySelectorAll("[data-codespan='js']");
+
+  for (const elem of codeElems) {
+    elem.innerHTML = jsAddSpans(elem.innerText, "js");
+  }
 }
--- tests/tests.js
+++ tests/tests.js
@@ -5,8 +5,8 @@ import {
   punctuations,
   quotes,
   brackets,
-} from "../src/definitions.js";
-import jsAddSpans from "../src/index.js";
+} from "../src/definitions-js.js";
+import { jsAddSpans } from "../src/index.js";
 
 const definitions = {
   keywords,
@@ -18,7 +18,7 @@ const definitions = {
 
 describe("Ensure definitions have the correct number of elements", () => {
   [
-    ["keywords", 48],
+    ["keywords", 44],
     ["declarators", 7],
     ["punctuations", 10],
     ["quotes", 3],
@@ -30,16 +30,47 @@ describe("Ensure definitions have the correct number of elements", () => {
   });
 });
 
-["brackets", "quotes"].forEach((typeGroup) => {
-  describe(`jsAddSpans(text) inserts the appropriate spans for ${typeGroup}`, () => {
-    definitions[typeGroup].forEach((value) => {
-      const classPostfix = typeGroup.slice(0, -1);
-      const expected = `<span class="codespan-${classPostfix}">${value}</span>`;
+["keywords", "declarators", "punctuations", "quotes", "brackets"].forEach(
+  (typeGroup) => {
+    describe(`jsAddSpans(text) inserts the appropriate spans for ${typeGroup}`, () => {
+      definitions[typeGroup].forEach((value) => {
+        const classPostfix = typeGroup.slice(0, -1);
+        const expected = `<span class="codespan-js-${classPostfix}">${value}</span>`;
 
-      test(`${value} yields ${expected}`, () => {
-        const html = jsAddSpans(value);
-        expect(html).toBe(expected);
+        test(`${value} yields ${expected}`, () => {
+          const html = jsAddSpans(value);
+          expect(html).toBe(expected);
+        });
       });
     });
+  }
+);
+
+describe(`jsAddSpans(text) transforms strings for char:`, () => {
+  quotes.forEach((quoteChar) => {
+    const text = `${quoteChar}This is a string.${quoteChar}`;
+    const expected = `<span class="codespan-js-quote">${quoteChar}</span><span class="codespan-js-string">This is a string.</span><span class="codespan-js-quote">${quoteChar}</span>`;
+
+    test(quoteChar + "  ", () => {
+      const html = jsAddSpans(text);
+      expect(html).toBe(expected);
+    });
+  });
+});
+
+describe(`jsAddSpans(text) transforms comments`, () => {
+  test(`single line comments are correct`, () => {
+    const text = `// this is a comment\n`;
+    const expected = `<span class="codespan-js-comment">// this is a comment</span><span class="codespan-js-newline">\n</span>`;
+    // const expected = `<span class="codespan-js-ws"> </span><span class="codespan-js-comment">// this is a comment</span><span class="codespan-js-ws">\n</span>`;
+    console.log("===================");
+    expect(jsAddSpans(text)).toBe(expected);
+  });
+
+  test(`block comments are correct`, () => {
+    const text = `/* this is a comment */`;
+    const expected = `<span class="codespan-js-comment">/* this is a comment */</span>`;
+
+    expect(jsAddSpans(text)).toBe(expected);
   });
 });
